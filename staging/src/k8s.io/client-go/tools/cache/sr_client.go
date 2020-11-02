2817e99b7344b330c2f7938726d767833e47fe52
marshtompsxd <xudongsunmarshtomp@gmail.com>
2020-11-02 15:45:36 -0600

    mutex

--- staging/src/k8s.io/client-go/tools/cache/sr_client.go
+++ staging/src/k8s.io/client-go/tools/cache/sr_client.go
@@ -4,34 +4,45 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"sync"
 
 	"k8s.io/klog"
 )
 
 var (
-	serverHostPort string = "localhost:1234"
-	// DeleteNodeScheduler controls whether delete the node at scheduler
-	DeleteNodeScheduler bool = true
+	mu                  sync.Mutex
+	serverHostPort      string = "localhost:1234"
+	deleteNodeScheduler bool   = true
 )
 
 // SRSendDeleteNode sends delete node to server
 func SRSendDeleteNode(node string) {
-	SRSend(serverHostPort, "[SR]\tDN\t"+node)
+	mu.Lock()
+	defer mu.Unlock()
+	if deleteNodeScheduler {
+		if SRSend(serverHostPort, "[SR]\tDN\t"+node) {
+			deleteNodeScheduler = false
+		}
+	}
 }
 
 // SRSend sends text to server
-func SRSend(hostPort string, text string) {
+func SRSend(hostPort string, text string) bool {
 	c, connectErr := net.Dial("tcp", hostPort)
 	if connectErr != nil {
 		klog.Errorf("[SR] failed to connect to SRServer: %v", connectErr)
-		return
+		return false
 	}
 	klog.Warningf("[SR] send %s to %s", text, hostPort)
 	fmt.Fprintf(c, text+"\n")
 	reply, readErr := bufio.NewReader(c).ReadString('\n')
 	if readErr != nil {
 		klog.Errorf("[SR] failed to hear back from SRServer: %v", readErr)
-		return
+		return false
 	}
 	klog.Warningf("[SR] hear back: %s", reply)
+	if reply == "YES" {
+		return true
+	}
+	return false
 }
